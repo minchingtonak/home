ac8eb0174c5dd76778326f86d7bff39db0217c8e
Alec <alec.minch@gmail.com>
2022-04-10 17:58:03 -0400

    EOL changes

--- src/config.ts
+++ src/config.ts
@@ -5,7 +5,7 @@ export const DEFAULT_TAB_TITLE = '🏠 Home';
 export const SEARCH_TAB_PREFIX = '🔍';
 export const NOTE_TAB_PREFIX = '📝';
 
-export const DEFAULT_SEARCH_URL = 'https://google.com/search';
+export const DEFAULT_SEARCH_URL = 'https://kagi.com/search';
 
 export const SITES_DATA_URL =
   'https://gist.githubusercontent.com/minchingtonak/b60356be980dc4f430fe73b523d1fafb/raw';
